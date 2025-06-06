9ea76e17e6b51d49e0353f4f40f2b51f560dd7b3
abc <abc@example.com>
2025-06-06 13:52:01 +0530

    For Brand Name given

--- src/main/java/com/learning/exkubedemo/Controller/greetController.java
+++ src/main/java/com/learning/exkubedemo/Controller/greetController.java
@@ -8,6 +8,6 @@ public class greetController {
 
     @GetMapping("/")
     public String greet() {
-        return "Hello World!! This is from Web App Service!!";
+        return "Hello World!! This is from Azure Web App Service created for FSSTech!!";
     }
 }
